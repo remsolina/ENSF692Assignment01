a6deda619ead580ce87902041757c587410e6a0b
Remi Oyediji <remioyediji@Remis-MBP.lan>
2024-05-10 00:01:49 -0600

    completed assignment instructions and added two new screenshots

---
+++ assignment1code.py
@@ -0,0 +1,11 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Remi Oyediji
+
+studentName = "Remi Oyediji"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+#print your best skill here
+print ("My best skill is playing the bass guitar.")
